961a6d8fe1886274a5aec875f410808e5b928e6a
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-03-26 11:47:00 -0400

    find by id

--- W25Week11DbFirstApproach/MainWindow.xaml.cs
+++ W25Week11DbFirstApproach/MainWindow.xaml.cs
@@ -20,9 +20,52 @@ namespace W25Week11DbFirstApproach
     /// </summary>
     public partial class MainWindow : Window
     {
+        // create an object of context class
+        SchoolDBEntities db = new SchoolDBEntities();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            LoadStudents();
+            LoadStandardsInCombobox();
+        }
+
+        private void LoadStudents()
+        {
+            var students = db.Students.ToList();
+            grdStudents.ItemsSource = students;
+        }
+
+        private void LoadStandardsInCombobox()
+        {
+            var standards = db.Standards.ToList();
+            cmbStandard.ItemsSource = standards;
+            cmbStandard.DisplayMemberPath = "StandardName";
+            cmbStandard.SelectedValuePath = "StandardId";
+        }
+
+        private void btnLoadStudents_Click(object sender, RoutedEventArgs e)
+        {
+            LoadStudents();
+        }
+
+        private void btnFind_Click(object sender, RoutedEventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var std = db.Students.Find(id);
+
+            if (std != null)
+            {
+                txtName.Text = std.StudentName;
+                cmbStandard.SelectedValue = std.StandardId;
+            }
+            else
+            {
+                txtName.Text = "";
+                cmbStandard.SelectedIndex = -1;
+                MessageBox.Show("Invalid ID. Please try again");
+            }
         }
     }
 }
